eb372f80dbcd68105303b83105f48f2581e19161
Drew <drew.deakin@gmail.com>
2014-10-22 23:18:51 -0400

    config update

--- config.php
+++ config.php
@@ -2,7 +2,21 @@
 
  // config.php
  // food-data
+
+ $DB_NAME = 'db';
+ $DB_HOST = $_ENV['DATABASE_SERVER'];
+ $DB_USER = 'user';
+ $DB_PASS = 'pass';
  
- echo $_SERVER['SERVER_ADDR'];
+ $mysqli = new mysqli( $DB_HOST, $DB_USER, $DB_PASS, $DB_NAME );
+ 
+ if ( mysqli_connect_errno( ) ) {
+ 
+  printf( "Connect failed: %s\n", mysqli_connect_error( ) );
+  exit( );
+  
+ }
+
+ define( 'PATH', dirname(__FILE__) . '/' );
  
 ?>
